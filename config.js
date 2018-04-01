020303bd207ca3c5fa6763076bd90d8d813f9eff
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-04-01 17:15:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'NDI5OTgzMzE2NDM1Nzk1OTc4.DaJksQ.KtZnfF9eGv2BvHA4lLxTDdB6DbA';
 
 exports.PREFIX = 'm!';
 
-exports.GOOGLE_API_KEY = 'ea8b23513993dd077cfb1e8a1a8dec9cd85c06b0';
+exports.GOOGLE_API_KEY = 'AIzaSyBY1T9JEXMLQRUsjy1qC4RkXIIkzoxHfvQ';
