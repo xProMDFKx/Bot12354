d9b3dcf57ad1b028b001d2165f5dea31d3a255c6
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-04-09 15:46:16 +0300

    Update discord.js

--- discord.js
+++ discord.js
@@ -168,7 +168,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
 if(message.content.startsWith('/mass')) {
-    if(message.author.id === "295952120400838657"){
+    if(message.author.id === "272074897839226884"){
         let args = message.content.split(" ").slice(1);
         var argresult = args.join(" ")
         const argsresult = args.join(" ")
